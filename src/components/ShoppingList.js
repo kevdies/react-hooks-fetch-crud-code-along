c4ad689408373ab94cf6017cd2c52f9e695d2dc3
Kevin Diesenberg <kdiesenb@gmail.com>
2022-11-25 17:10:26 -0500

    handle patch anddelete

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -13,6 +13,23 @@ function ShoppingList() {
       .then((items) => setItems(items));
   }, []);
 
+  function handleDeleteItem(deletedItem) {
+    const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+    setItems(updatedItems);
+  }
+
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      }
+      else {
+        return item;
+      }
+    })
+    setItems(updatedItems);
+  }
+
   function handleAddItem(newItem) {
     setItems(...items, newItem);
     }
@@ -35,7 +52,12 @@ function ShoppingList() {
       />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item
+            key={item.id}
+            item={item}
+            onUpdateItem={handleUpdateItem}
+            onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
