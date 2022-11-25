16eeb9ffb4657aa85be39354c68201e330c2ff49
Kevin Diesenberg <kdiesenb@gmail.com>
2022-11-25 15:22:58 -0500

    display items

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -7,9 +7,16 @@ function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
-  }
+  useEffect(() => {
+    fetch("http://localhost:4000/items")
+      .then((res) => res.json())
+      .then((items) => setItems(items));
+  }, []);
+
+    function handleCategoryChange(category) {
+      setSelectedCategory(category);
+    }
+
 
   const itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
